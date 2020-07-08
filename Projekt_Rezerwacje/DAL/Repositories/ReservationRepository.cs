b14ea1a8ad5fb8192482e3f00305c30be28a0233
bartosz <bartoszhajduk0@gmail.com>
2020-07-08 17:51:17 +0200

    REVERSE CARD

--- Projekt_Rezerwacje/DAL/Repositories/ReservationRepository.cs
+++ Projekt_Rezerwacje/DAL/Repositories/ReservationRepository.cs
@@ -11,18 +11,15 @@ namespace Projekt_Rezerwacje.DAL.Repositories
 
     class ReservationRepository
     {
-        private const string ADD_RESERVATION = "INSERT INTO `rezerwacje`(`id_k`, `od`, `do`) VALUES ";
-        private const string ADD_RESERVATION_ROOM = "INSERT INTO `pokoje_rezerwacje`(`id_r`, `id_p`) VALUES ";
-
         public static List<Reservation> GetReservations(int id_p)
         {
-            string ROOM_RESERVATIONS = "SELECT * FROM rezerwacje, klienci, pokoje_rezerwacje  " +
+            string ALL_RESERVATIONS = "SELECT * FROM rezerwacje, klienci, pokoje_rezerwacje  " +
                 $"WHERE rezerwacje.id_k = klienci.id_k AND rezerwacje.id_r = pokoje_rezerwacje.id_r AND pokoje_rezerwacje.id_p = {id_p}";
 
             List<Reservation> reservations = new List<Reservation>();
             using (var connection = DBConnection.Instance.Connection)
             {
-                MySqlCommand command = new MySqlCommand(ROOM_RESERVATIONS, connection);
+                MySqlCommand command = new MySqlCommand(ALL_RESERVATIONS, connection);
                 connection.Open();
                 var reader = command.ExecuteReader();
                 while (reader.Read())
@@ -44,29 +41,6 @@ namespace Projekt_Rezerwacje.DAL.Repositories
                 var n = command.ExecuteNonQuery();
                 if (n == 1) state = true;
 
-                connection.Close();
-            }
-            return state;
-        }
-
-        public static bool AddReservation(Reservation reservation, int id_p)
-        {
-            bool state = false;
-            using (var connection = DBConnection.Instance.Connection)
-            {
-                MySqlCommand command = new MySqlCommand($"{ADD_RESERVATION} {reservation.ToInsert()}", connection);
-                connection.Open();
-                var id = command.ExecuteNonQuery();
-                reservation.ID = (int)command.LastInsertedId;
-                connection.Close();
-            }
-            using (var connection = DBConnection.Instance.Connection)
-            {
-                MySqlCommand command = new MySqlCommand($"{ADD_RESERVATION_ROOM} ({reservation.ID}, {id_p})", connection);
-                connection.Open();
-                var id = command.ExecuteNonQuery();
-                state = true;
-                reservation.ID = (int)command.LastInsertedId;
                 connection.Close();
             }
             return state;
