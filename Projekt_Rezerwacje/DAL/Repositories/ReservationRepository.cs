350c4c6250f8ccad343feec157dbc032dcf5d7da
bartosz <bartoszhajduk0@gmail.com>
2020-07-08 17:16:39 +0200

    Dodanie usuwania rezerwacji

--- Projekt_Rezerwacje/DAL/Repositories/ReservationRepository.cs
+++ Projekt_Rezerwacje/DAL/Repositories/ReservationRepository.cs
@@ -16,17 +16,34 @@ namespace Projekt_Rezerwacje.DAL.Repositories
             string ALL_RESERVATIONS = "SELECT * FROM rezerwacje, klienci, pokoje_rezerwacje  " +
                 $"WHERE rezerwacje.id_k = klienci.id_k AND rezerwacje.id_r = pokoje_rezerwacje.id_r AND pokoje_rezerwacje.id_p = {id_p}";
 
-            List<Reservation> clients = new List<Reservation>();
+            List<Reservation> reservations = new List<Reservation>();
             using (var connection = DBConnection.Instance.Connection)
             {
                 MySqlCommand command = new MySqlCommand(ALL_RESERVATIONS, connection);
                 connection.Open();
                 var reader = command.ExecuteReader();
                 while (reader.Read())
-                    clients.Add(new Reservation(reader));
+                    reservations.Add(new Reservation(reader));
                 connection.Close();
             }
-            return clients;
+            return reservations;
+        }
+
+        public static bool DeleteReservation(int reservationID)
+        {
+            bool state = false;
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                string DELETE_RESERVATION = $"DELETE FROM pokoje_rezerwacje WHERE id_r={reservationID}; DELETE FROM rezerwacje WHERE id_r ={ reservationID}";
+              
+                MySqlCommand command = new MySqlCommand(DELETE_RESERVATION, connection);
+                connection.Open();
+                var n = command.ExecuteNonQuery();
+                if (n == 1) state = true;
+
+                connection.Close();
+            }
+            return state;
         }
     }
 }
